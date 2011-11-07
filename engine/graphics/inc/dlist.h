05b59742fff3c15320df79d07185049d9b0d07c1
Nikola Georgiev <georgiev.nikito@gmail.com>
2011-11-07 18:14:33 +0200

    Fixed double linked list
    Signed-off-by: Nikola Georgiev <georgiev.nikito@gmail.com>

--- engine/graphics/inc/dlist.h
+++ engine/graphics/inc/dlist.h
@@ -1,6 +1,10 @@
 #ifndef DLIST_H
 #define DLIST_H
+
 #include <stdlib.h>
+
+#include <stdlib.h>
+#include <string.h>
 
 template <typename T>
 class double_linked
@@ -47,4 +51,86 @@ public:
         }
     }
 };
+
+template <typename T>
+void double_linked<T>::push_back(T data)
+{
+    tail = new node(data, tail, NULL);
+    if( tail->prev )
+        tail->prev->next = tail;
+
+    if( empty() )
+        head = tail;
+}
+
+template <typename T>
+void double_linked<T>::push_front(T data)
+{
+    head = new node(data, NULL, head);
+    if( head->next )
+        head->next->prev = head;
+
+    if( empty() )
+        tail = head;
+}
+
+template<typename T>
+T double_linked<T>::pop_back()
+{
+    if( empty() )
+        throw("double_linked : list empty");
+    node* temp(tail);
+    T data( tail->data );
+    tail = tail->prev ;
+
+    if( tail )
+        tail->next = NULL;
+    else
+        head = NULL ;
+
+    delete temp;
+    return data;
+}
+
+template<typename T>
+T double_linked<T>::pop_front()
+{
+    if( empty() )
+        throw("double_linked : list empty");
+    node* temp(head);
+    T data( head->data );
+    head = head->next ;
+
+    if( head )
+        head->prev = NULL;
+    else
+        tail = NULL;
+
+    delete temp;
+    return data;
+}
+
+template<typename T>
+T double_linked<T>::get_next()
+{
+    static node* curNode;
+    if (!curNode)
+    {
+        curNode = head;
+    }
+    else
+    {
+        curNode = curNode->next;
+    }
+    return curNode->data;
+}
+
+template<typename T>
+T double_linked<T>::get_head()
+{
+    return this->head->data;
+}
+
+
+
 #endif // DLIST_H
